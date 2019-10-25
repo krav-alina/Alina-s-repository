c9edae9661e4e4a9d9bcc89015ef23e590ccf3ff
krav-alina <56936129+krav-alina@users.noreply.github.com>
2019-10-25 22:53:04 +0300

    Add files via upload

---
+++ задание1.py
@@ -0,0 +1,11 @@
+# задание 1
+# какие из этих строк можно конкатенировать? Какие умножать? Какие вычитать? Запишите все результаты, которые у Вас получились
+a = 23
+b = 34.02
+c = "python is cool"
+d = "you are cool, too"
+print(c+d)
+#python is coolyou are cool, too, 57.02,-11.020000000000003, 11.020000000000003,782.46,0.6760728982951204,1.4791304347826089
+#python is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is coolpython is cool
+#you are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are cool, tooyou are c
+print(d*b)
